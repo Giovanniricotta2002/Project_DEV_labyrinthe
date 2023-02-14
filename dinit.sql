9b42b50ebe0059c38351fc430161695993869ad4
Giovanni <115021221+Giovanniricotta2002@users.noreply.github.com>
2023-02-14 09:31:31 +0100

    init.sql uodate

--- dinit.sql
+++ dinit.sql
@@ -11,34 +11,47 @@ CREATE TABLE ia (
 
 CREATE TABLE texture
 (
-    id_texture smallserial PRIMARY KEY NOT NULL nextval('labyrinthe_texture_primary_key'),
+    id_texture serial PRIMARY KEY NOT NULL,
     nom TEXT
 );
 
-CREATE TABLE case
+CREATE TABLE caseblock
 (
-    id_case serial PRIMARY KEY NOT NULL nextval('labyrinthe_case_primary_key'),
-    id_map serial,
+    id_case serial PRIMARY KEY NOT NULL,
+    id_map int,
+    id_texture int,
     x INT,
-    y INT
+    y INT,
+    CONSTRAINT idmap
+      FOREIGN KEY(id_map) 
+	  REFERENCES map(id_map)
+    CONSTRAINT idtexture
+        FOREIGN KEY(id_texture)
+        REFERENCES texture(id_texture)
 );
+
+
 
 CREATE TABLE map
 (
-    id_map INT PRIMARY KEY NOT NULL nextval('labyrinthe_map_primary_key'),
+    id_map INT PRIMARY KEY NOT NULL,
+    id_diff serial,
     nom TEXT,
     creator TEXT,
     date_creation TIMESTAMP,
     date_modification TIMESTAMP,
-    date_test TIMESTAMP
+    date_test TIMESTAMP,
+    CONSTRAINT iddiff
+      FOREIGN KEY(id_diff)
+      REFERENCES diff(id_difficulte)
 );
+
 
 CREATE TABLE diff
 (
-    id_difficulte INT PRIMARY KEY NOT NULL nextval('labyrinthe_difficulte_primary_key'),
+    id_difficulte INT PRIMARY KEY NOT NULL,
     nbr_de_case_total INT,
     name_difficulte char
-
 );
 
 
@@ -46,3 +59,5 @@ ALTER SEQUENCE labyrinthe_texture_primary_key OWNED BY texture.id_texture;
 ALTER SEQUENCE labyrinthe_case_primary_key OWNED BY case.id_case;
 ALTER SEQUENCE labyrinthe_map_primary_key OWNED BY map.id_map;
 ALTER SEQUENCE labyrinthe_difficulte_primary_key OWNED BY diff.id_difficulte;
+
+
--- init.sh
+++ init.sh
@@ -16,4 +16,4 @@ echo exporté la base
 
 echo Start docker img
 echo \# "docker run -v /bdd:/var/lib/postgresql/data --name bdd -e POSTGRES_PASSWORD=bdd -p 5432:5432 postgres:bullseye"
-docker run -v /bdd:/var/lib/postgresql/data --name bdd -e POSTGRES_PASSWORD=bdd -p 5432:5432 postgres:bullseye
+docker run -v $PWD/bdd:/var/lib/postgresql/data --name bdd -e POSTGRES_PASSWORD=bdd -p 5432:5432 postgres:bullseye
